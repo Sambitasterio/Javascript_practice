7f1255114b1bdfd49f397527b1220cf8e2496618
Sambit Behera <sambit.behera8587@gmail.com>
2026-06-06 20:22:14 +0530

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,8 @@
+/*console.log("2">1); // it will give true because 2 is greater than 1
+console.log("2"<1); // it will give false because 2 is not less than 1
+console.log("2"==2); // it will give true because when we use the == operator, JavaScript will convert the string to a number before comparing them
+console.log("2"===2); // it will give false because when we use the === operator, JavaScript will not convert the string to a number and will compare them as they are, so they are not strictly equal
+*/
+console.log(null>0); // it will give false because null is not greater than 0
+console.log(null==0);// it will give false because when we use the == operator, JavaScript will not convert null to a number and will compare them as they are, so they are not equal
+console.log(null>=0);// it will give true because when we use the >= operator, JavaScript will convert null to a number and will compare them, so null is converted to 0 and 0 is greater than or equal to 0
