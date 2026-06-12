c1df078ae98c8b31247ed9db54127398e5e8aeea
Sambit Behera <sambit.behera8587@gmail.com>
2026-06-12 19:25:42 +0530

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "sambit";
+const repoCount = 11
+
+console.log(name + repoCount); 
+
+console.log(`My name is ${name} and I have ${repoCount} reositories`);
+
+const game = new String('sambit');
+
+console.log(game[2]);
+console.log(game.length);
+console.log(game.toUpperCase());
+console.log(game.charAt(2));
+console.log(game.indexOf('a'));
+
+const newString = game.substring(0, 3);
+console.log(newString);
