a6374031383e8c22d8c2aadc8e8abeff800bb0db
Sambit Behera <sambit.behera8587@gmail.com>
2026-06-23 10:01:17 +0530

    Arrays

---
+++ 02_basics/01_array.js
@@ -0,0 +1,15 @@
+const myarr = [0,1,2,3,4];
+const myheros = ["Iron man", "thor", "spiderman", "hulk"];
+const myarr2 = new Array(1,2,3,4,5,6);
+
+//console.log(myarr[0]);
+//console.log(myarr2);
+
+//myarr.push(5);
+//myarr.push(6);
+//myarr.pop();
+console.log("A",myarr);
+const my1 = myarr.slice(1,3);
+console.log("B",my1);
+const my2 = myarr.splice(1,3);
+console.log("C",my2);
---
+++ 02_basics/02_array.js
@@ -0,0 +1,5 @@
+const marvel_heroes = ["Iron man", "thor", "spiderman", "hulk"];
+const dc_heroes = ["superman", "flash", "batman", "aquaman"];
+
+const newHeroes = marvel_heroes.concat(dc_heroes);
+console.log(newHeroes);
