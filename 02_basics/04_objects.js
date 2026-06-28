6bb9029a6bcd41906229e70951ab9f06719f8a14
Sambit Behera <sambit.behera8587@gmail.com>
2026-06-28 17:35:55 +0530

    Objects and functions

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,9 @@
+const course = {
+    courseName : "JavaScript",
+    price : "999",
+    CourseInstructor: "Sam"
+}
+
+const {CourseInstructor:instructor} = course;
+console.log(instructor);
+
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,9 @@
+function saymyname(){
+    console.log("S");
+    console.log("A");
+    console.log("M");
+    console.log("B");
+    console.log("I");
+    console.log("T");
+}
+saymyname();
