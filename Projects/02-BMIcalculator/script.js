fb6b237c4281b970e6cf3928aba60696248eb0d9
Sambit Behera <sambit.behera8587@gmail.com>
2026-07-04 13:52:34 +0530

    BMI calculator

---
+++ Projects/02-BMIcalculator/script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+    
+    const height = parseFloat(document.querySelector('#height').value);
+    const weight = parseFloat(document.querySelector('#weight').value);
+    const results= document.querySelector('#results');
+
+    if(height === '' || height< 0 || isNaN(height)){
+        results.innerHTML = "Please provide a valid height";
+    }
+    if(weight === '' || weight<0 || isNaN(weight)){
+        results.innerHTML = "Please provide a valid weight";
+    }
+    else{
+        const bmi = (weight/(height/100)**2).toFixed(2);
+        results.innerHTML = `Your BMI is ${bmi}`;
+    }
+});
+    
+
+    
