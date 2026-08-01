7a31c293ff76e86b3f96dde3dee03a03074acf06
Sambit Behera <sambit.behera8587@gmail.com>
2026-08-01 19:22:38 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -11,14 +11,14 @@ promiseOne.then(function(){
     console.log("Promise consumed");
 })
 
-new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log("Async task 2");
-        resolve()
-    },1000)
-}).then(function(){
-    console.log("Async 2 resolved");
-})
+// new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log("Async task 2");
+//         resolve()
+//     },1000)
+// }).then(function(){
+//     console.log("Async 2 resolved");
+// })
 
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
